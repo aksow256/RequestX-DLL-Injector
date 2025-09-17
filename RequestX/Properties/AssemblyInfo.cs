c6e9a9849dafbf70ba941d1f4809c39f2cf274ba
december8968 <snowdrop3436@outlook.com>
2025-09-17 12:43:27 -0700

    Upload Files

---
+++ RequestX/Properties/AssemblyInfo.cs
@@ -0,0 +1,15 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+[assembly: AssemblyTitle("RequestX")]
+[assembly: AssemblyDescription("DLL Injector")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("DLL Injector")]
+[assembly: AssemblyCopyright("RequestX Copyright © 2024")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+[assembly: ComVisible(false)]
+[assembly: Guid("a03485c1-f807-44c5-b4ab-945f1f0bbab7")]
+[assembly: AssemblyVersion("2.7.0.0")]
+[assembly: AssemblyFileVersion("2.7.0.0")]
